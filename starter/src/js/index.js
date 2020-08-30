7536d60451c313556fa038e1582eab353cbfe968
sungwoo <54757435+sung-woo-jang@users.noreply.github.com>
2020-08-30 19:49:22 +0900

    setting fin.
    webpack, npm, babel, scripts 뭐뭐 씨발 아주 뭐 많기도 하다. 뭔 소린지 하나도 모르겠어~~~

--- starter/src/js/index.js
+++ starter/src/js/index.js
@@ -1,4 +1,11 @@
 // Global app controller
-import num from './test';
+import str from './models/Search';
+// import { add as a, multiply as m, ID } from './views/searchView';
+import * as serchView from './views/searchView';
 
-console.log(`I imported ${num} from another module`);
+console.log(
+    `Using imported function! ${serchView.add(
+        serchView.ID,
+        2
+    )} and ${serchView.multiply(3, 5)}, ${str}`
+);
--- starter/webpack.config.js
+++ starter/webpack.config.js
@@ -1,7 +1,29 @@
 const path = require('path');
-// 현재 폴더의 절대경로
+// 현재 폴더의 절대경로(이것은 내장 노드 패키지를 사용한 구문)
+const htmlWebpackPlugin = require(`html-webpack-plugin`);
 
 module.exports = {
-    entry: './src/js/index.js',
-    output: { path: path.resolve(__dirname, 'dist/js'), filename: 'bundle.js' },
+    entry: ['babel-polyfill', './src/js/index.js'],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/bundle.js',
+    },
+    devServer: {
+        contentBase: './dist',
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html',
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: { loader: 'babel-loader' },
+            },
+        ],
+    },
 };
