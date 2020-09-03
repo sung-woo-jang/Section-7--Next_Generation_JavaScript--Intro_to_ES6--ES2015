a3e462cfcff0495d27cd2a4cdea0a9912d2ed4ab
seastory624@gmail.com <54757435+sung-woo-jang@users.noreply.github.com>
2020-09-03 15:04:56 +0900

    Implementing Search Results Pagination
    1. 로딩 그 빙빙 돌아가는거 구현함
    2. 검색결과 구현한 것들을 Math.ceil(num / numRes)로 나눠서 조금씩만 보이게하고, 또 다음 페이지로 가는 버튼을 구현함(pagination implementing)

--- starter/src/js/index.js
+++ starter/src/js/index.js
@@ -1,6 +1,6 @@
 import Search from './models/Search';
 import * as searchView from './views/searchView';
-import { elements } from './views/base';
+import { elements, renderLoader, clearLoader } from './views/base';
 
 /**
  * - Search object
@@ -13,19 +13,22 @@ const state = {};
 const controlSearch = async () => {
     // 1. Get query from view
     const query = searchView.getInput();
+    // 검색창에 넣은 값
 
     if (query) {
         // 2. New search object and add to state
         state.search = new Search(query);
 
         // 3. Prepare UI for results
-        searchView.clearInput();
-        searchView.clearResults();
+        searchView.clearInput(); // 검색창 지우가
+        searchView.clearResults(); // 새로 검색하면 그 전 Data 삭제
+        renderLoader(elements.searchRes); //로딩중인 이미지 띄우기
 
         // 4. Search for recipes
         await state.search.getResults();
 
         // 5. Render results on UI
+        clearLoader();
         searchView.renderResults(state.search.result);
     }
 };
@@ -33,4 +36,13 @@ const controlSearch = async () => {
 elements.searchForm.addEventListener('submit', (e) => {
     e.preventDefault();
     controlSearch();
+});
+
+elements.searchResPages.addEventListener('click', (e) => {
+    const btn = e.target.closest('.btn-inline');
+    if (btn) {
+        const goToPage = parseInt(btn.dataset.goto, 10);
+        searchView.clearResults(); // 새로 검색하면 그 전 Data 삭제
+        searchView.renderResults(state.search.result, goToPage);
+    }
 });
